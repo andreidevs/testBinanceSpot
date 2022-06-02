09f2d2b76dd9da76037cf281880423f497a3d72e
andrei_dev <andrei.dev2016@yandex.ru>
2022-06-02 21:37:24 +0600

    deploy

---
+++ .nuxt/dist/server/components/table.js
@@ -0,0 +1,135 @@
+exports.ids = [3];
+exports.modules = {
+
+/***/ 26:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/Table.vue?vue&type=template&id=b1dce8c8&scoped=true&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticStyle:{"width":"100%"}},[_vm._ssrNode("<div class=\"ml-2\" data-v-b1dce8c8>","</div>",[_vm._t("default")],2),_vm._ssrNode(" <div class=\"wrapTable\" data-v-b1dce8c8><table class=\"table\" data-v-b1dce8c8><thead data-v-b1dce8c8><tr data-v-b1dce8c8><th data-v-b1dce8c8>Price</th> <th data-v-b1dce8c8>Ammount</th> <th class=\"hide\" data-v-b1dce8c8>Total</th></tr></thead> <tbody data-v-b1dce8c8>"+(_vm._ssrList((_vm.items),function(item,index){return ("<tr data-v-b1dce8c8><td data-v-b1dce8c8>"+_vm._ssrEscape(_vm._s(item[0]))+"</td> <td data-v-b1dce8c8>"+_vm._ssrEscape(_vm._s(item[1]))+"</td> <td class=\"hide\" data-v-b1dce8c8>"+_vm._ssrEscape("\n            "+_vm._s(item[0] * item[1])+"\n          ")+"</td></tr>")}))+"</tbody></table></div>")],2)}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./components/Table.vue?vue&type=template&id=b1dce8c8&scoped=true&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/Table.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var Tablevue_type_script_lang_js_ = ({
+  name: "MainTable",
+  props: ["items"]
+});
+// CONCATENATED MODULE: ./components/Table.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_Tablevue_type_script_lang_js_ = (Tablevue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(3);
+
+// CONCATENATED MODULE: ./components/Table.vue
+
+
+
+function injectStyles (context) {
+  
+  var style0 = __webpack_require__(28)
+if (style0.__inject__) style0.__inject__(context)
+
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_Tablevue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  "b1dce8c8",
+  "6470542b"
+  
+)
+
+/* harmony default export */ var Table = __webpack_exports__["default"] = (component.exports);
+
+/* nuxt-component-imports */
+installComponents(component, {Table: __webpack_require__(26).default})
+
+
+/***/ }),
+
+/***/ 27:
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(29);
+if(content.__esModule) content = content.default;
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add CSS to SSR context
+var add = __webpack_require__(5).default
+module.exports.__inject__ = function (context) {
+  add("f019d244", content, true, context)
+};
+
+/***/ }),
+
+/***/ 28:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_nuxt_postcss8_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_nuxt_postcss8_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Table_vue_vue_type_style_index_0_id_b1dce8c8_scoped_true_lang_scss___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(27);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_nuxt_postcss8_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_nuxt_postcss8_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Table_vue_vue_type_style_index_0_id_b1dce8c8_scoped_true_lang_scss___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_nuxt_postcss8_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_nuxt_postcss8_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Table_vue_vue_type_style_index_0_id_b1dce8c8_scoped_true_lang_scss___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_nuxt_postcss8_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_nuxt_postcss8_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Table_vue_vue_type_style_index_0_id_b1dce8c8_scoped_true_lang_scss___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_nuxt_postcss8_node_modules_css_loader_dist_cjs_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_nuxt_postcss8_node_modules_postcss_loader_dist_cjs_js_ref_7_oneOf_1_2_node_modules_sass_loader_dist_cjs_js_ref_7_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Table_vue_vue_type_style_index_0_id_b1dce8c8_scoped_true_lang_scss___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+
+
+/***/ }),
+
+/***/ 29:
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(4);
+var ___CSS_LOADER_EXPORT___ = ___CSS_LOADER_API_IMPORT___(function(i){return i[1]});
+// Module
+___CSS_LOADER_EXPORT___.push([module.i, "table[data-v-b1dce8c8]{font-size:14px;border-radius:10px;border-spacing:0;text-align:center;width:100%}th[data-v-b1dce8c8]{background:#108ee9;color:#fff;padding:10px 20px;position:-webkit-sticky;position:sticky;top:0}td[data-v-b1dce8c8],th[data-v-b1dce8c8]{border-color:#fff;border-style:solid;border-width:0 1px 1px 0}td[data-v-b1dce8c8]:first-child,th[data-v-b1dce8c8]:first-child{text-align:left}th[data-v-b1dce8c8]:first-child{border-top-left-radius:10px}th[data-v-b1dce8c8]:last-child{border-top-right-radius:10px;border-right:none}td[data-v-b1dce8c8]{padding:10px 20px;background:#f8e391}tr:last-child td[data-v-b1dce8c8]:first-child{border-radius:0 0 0 10px}tr:last-child td[data-v-b1dce8c8]:last-child{border-radius:0 0 10px 0}tr td[data-v-b1dce8c8]:last-child{border-right:none}.wrapTable[data-v-b1dce8c8]{height:calc(100vh - 120px);overflow:auto;width:100%}@media(max-width:840px){.hide[data-v-b1dce8c8]{display:none}th[data-v-b1dce8c8]:nth-child(2){border-top-right-radius:10px}}@media(max-width:1090px){table[data-v-b1dce8c8]{font-size:12px}td[data-v-b1dce8c8]{padding:5px 10px}}@media(max-width:440px){table[data-v-b1dce8c8]{font-size:10px;padding:2px 5px!important}}@media(max-width:400px){table[data-v-b1dce8c8]{font-size:8px}}", ""]);
+// Exports
+___CSS_LOADER_EXPORT___.locals = {};
+module.exports = ___CSS_LOADER_EXPORT___;
+
+
+/***/ })
+
+};;
+//# sourceMappingURL=table.js.map
