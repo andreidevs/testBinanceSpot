3a89e45d37f38f5fb133c33c43911ed222830855
andrei_dev <andrei.dev2016@yandex.ru>
2022-06-02 21:09:26 +0600

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/testBinanceSpot/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
